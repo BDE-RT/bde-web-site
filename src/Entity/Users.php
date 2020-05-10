c40221ecc239573bddef1ebc554e0a041a52af7f
Thomas-PC <thomas8433.vitre@gmail.com>
2020-05-10 18:16:46 +0200

    Edit Users

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -9,7 +9,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un compte avec cette e-mail")
+ * @UniqueEntity(fields={"username"}, message="Il existe déjà un compte avec ce nom d'utilisateur")
  */
 class Users implements UserInterface
 {
